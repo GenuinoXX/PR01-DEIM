06be087de724a1a3e8bde0743892d6cf40262639
GenuinoXX <maurisebas2000@gmail.com>
2021-09-24 17:58:23 +0200

    Ejercicio 3.3 y 3.3.2

--- Ejercicio1/Assets/NewBehaviourScript.cs
+++ Ejercicio1/Assets/NewBehaviourScript.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    int n = 5;
-    int m = 8;
+    int n = 0;
+    int m = 0;
+    int ñ = 0;
     string mens = "El bayonetta está guapo";
     string mens2 = "Odio a Chris Pratt";
     public string nombre = "Rodolfo";
     public bool Penta = true;
-
-    int multiplicar (int n , int m)
-    {
-        int resultado = n * m;
-        return resultado;
-    }
+    public Text timer;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        int resultado = multiplicar(5, 8);
-        print(resultado);
-        if (resultado != 15)
+        StartCoroutine("Contador");
+    }
+
+    IEnumerator Contador()
+    {
+        while (true)
         {
-            bool Penta = false;
-            print(Penta);
+            print("la hora es "+ ñ + ":"+m+ ":" + n);
+            timer.text = "la hora es " + ñ + ":" + m + ":" + n;
+            n++;
+            yield return new WaitForSeconds(1f);
+
+            if (n == 60)
+            {
+                n = 0;
+                m++;
+            }
+
+            if (m == 60)
+            {
+                m = 0;
+                ñ++;
+            }
+            if (ñ == 1)
+            {
+                StopCoroutine("Contador");
+            }
         }
     }
 
