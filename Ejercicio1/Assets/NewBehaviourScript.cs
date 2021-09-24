36858ce7b7691c2b69899410b1d69895e39654ee
GenuinoXX <maurisebas2000@gmail.com>
2021-09-24 16:14:42 +0200

    Ejercicio 2.2 y 2.3

--- Ejercicio1/Assets/NewBehaviourScript.cs
+++ Ejercicio1/Assets/NewBehaviourScript.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     int n = 5;
+    int m = 8;
+    string mens = "El bayonetta está guapo";
+    string mens2 = "Odio a Chris Pratt";
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        while (n < 25)
+        {
+            n++;
+            print(n);
+            if (n == 25)
+            {
+                print(mens);
+            }
+        }
+        for (m = 8; m <= 48; m++)
+        {
+            print(m);
+            if (m == 48)
+            {
+                print(mens2);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        n++;
-        if (n <= 15)
-        {
-            print (n);
-        }
+       
     }
 }
