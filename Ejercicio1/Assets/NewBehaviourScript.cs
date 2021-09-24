f6e25447ce4a669d9391332584966e054d5b9ea2
GenuinoXX <maurisebas2000@gmail.com>
2021-09-24 16:02:38 +0200

    Ejercicio 2.1

--- Ejercicio1/Assets/NewBehaviourScript.cs
+++ Ejercicio1/Assets/NewBehaviourScript.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public int NumEnt = 5;
-    public int NumEnt2 = 10;
-    public float NumDem = 4.25f;
-    public float NumDem2 = 3.50f;
-    public bool Bool = true;
-    public string Cadena = "Tu me dejaste de querer cuando te necesitaaaba";
-    public string Cadena2 = "Cuando más te queríiiiiiia";
+    int n = 5;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        print(NumEnt + NumEnt2);
-        print(NumDem + NumDem2);
-        print(Bool);
-        print(Cadena + " " + Cadena2);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        n++;
+        if (n <= 15)
+        {
+            print (n);
+        }
     }
 }
